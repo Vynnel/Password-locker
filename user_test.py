88592364ef2f88a91d8111131498c2305d3550af
Vynnel Lee <vynnellee@gmail.com>
2018-11-02 16:35:37 +0300

    created a test to check if a user's credentials are saved successfully.

--- user_test.py
+++ user_test.py
@@ -20,4 +20,11 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.username, "Vynnel")
         self.assertEqual(self.new_user.password, "rock1ville")
 
-    
+    def test_save_user(self):
+        """
+        test_save_user test case to test if the user object is saved into the users' list
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
+
+   
