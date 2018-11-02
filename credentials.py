0d89ace9bbfdbb802ac52621b78b7d87af9b4d78
Vynnel Lee <vynnellee@gmail.com>
2018-11-02 16:25:12 +0300

    created a function that displays user credentials when prompted.

--- credentials.py
+++ credentials.py
@@ -19,4 +19,9 @@ class Credential:
         """
         Credential.credential_list.append(self)
 
-    
+    @classmethod
+    def display_credentials(cls):
+        """
+        display_credentials method that returns the credential list
+        """
+        return cls.credential_list
