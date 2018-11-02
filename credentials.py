6e872c708218b264b05b29c73cc306682f4a562b
Vynnel Lee <vynnellee@gmail.com>
2018-11-02 16:23:58 +0300

    created a function to save user credentials.

--- credentials.py
+++ credentials.py
@@ -13,4 +13,10 @@ class Credential:
         self.login = login
         self.password = password
 
+    def save_credential(self):
+        """
+        save_credential method saves credential objects into the credential_list
+        """
+        Credential.credential_list.append(self)
+
     
