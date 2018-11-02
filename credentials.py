ea18dedb7e6b126d55f34c2f61578397f01a8f8c
Vynnel Lee <vynnellee@gmail.com>
2018-11-02 16:22:14 +0300

    created the credentials class and a function that creates the user's credentials

---
+++ credentials.py
@@ -0,0 +1,16 @@
+import random
+
+
+class Credential:
+    """
+    Class that generates new instances of user's credentials
+    """
+    credential_list = []
+
+    def __init__(self, view_password, account, login, password):
+        self.view_password = view_password
+        self.account = account
+        self.login = login
+        self.password = password
+
+    
