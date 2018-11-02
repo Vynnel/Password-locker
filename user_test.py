bb8ddc57f7141021a5f6044483b9fdafac9eb71d
Vynnel Lee <vynnellee@gmail.com>
2018-11-02 16:31:58 +0300

    created a test to check if the user information is initialized properly

--- user_test.py
+++ user_test.py
@@ -13,4 +13,11 @@ class TestUser(unittest.TestCase):
         """
         self.new_user = User("Vynnel", "rock1ville")
 
-  
+    def test_init(self):
+        """
+        test_init test case to test if the object is initialized properly
+        """
+        self.assertEqual(self.new_user.username, "Vynnel")
+        self.assertEqual(self.new_user.password, "rock1ville")
+
+    
