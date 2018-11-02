fa947ca62fdfd9a461799bdd234d2d2f0721c4d5
Vynnel Lee <vynnellee@gmail.com>
2018-11-02 17:33:27 +0300

    setup a method to run before each test.

--- credentials_test.py
+++ credentials_test.py
@@ -1,3 +1,16 @@
 import unittest
 from credentials import Credential
 
+
+class TestCredential(unittest.TestCase):
+    """
+    Test class that defines test cases for the credential class behaviours
+    """
+
+    def setUp(self):
+        """
+        set up method to run before each test cases
+        """
+        self.new_credential = Credential("rock1ville", "Instagram", "Vynnel", "rock1ville")
+
+   
